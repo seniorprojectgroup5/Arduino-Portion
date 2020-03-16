d9878a59a1f80ae99de408076e3b5aaa955dc88f
Jonah <46599996+JonahJana@users.noreply.github.com>
2020-03-16 15:23:07 -0400

    Add files via upload
    proper comments this time that actually reflect the code

--- BluetoothTesting.ino
+++ BluetoothTesting.ino
@@ -9,11 +9,8 @@ millisDelay v_Delay;
 long int Data;
 char B[1];
 
-// vibrations:descriptors
-// 0:stop, 7:softest, 47:med, 24:sharp, 4:hard click, 1:hardest
-
 // inputs:vibrations
-// 0:do nothing, 1:7, 2:47, 3:24, 4:4, 5:1
+// 0:do nothing, 1:47, 2:24, 3:4, 4:7, 5:1
 void readSerialInput() {
   Serial.readBytes(B, 1);
   long int temp = B[0] - '0';
